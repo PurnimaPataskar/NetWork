7b8325fcdb18a27680f5366b34d4329cc379098f
Purnima Pataskar <purnima.pataskar@hcl.com>
2026-01-04 07:08:30 +0530

    Initial commit

---
+++ backend/src/main/java/com/networks/networks/NetWorksApplication.java
@@ -0,0 +1,13 @@
+package com.networks.networks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetWorksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/networks/networks/NetWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.networks.networks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
