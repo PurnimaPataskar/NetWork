ceda9365fe8ff94c5fe8a2a111f4b641f6664c1d
PurnimaPataskar <pataskar.purnima@gmail.com>
2026-02-20 21:58:21 +0530

    encoding password

--- backend/src/main/java/com/linkedin/backend/configuration/LoadDatabaseConfiguration.java
+++ backend/src/main/java/com/linkedin/backend/configuration/LoadDatabaseConfiguration.java
@@ -6,14 +6,21 @@ import org.springframework.context.annotation.Configuration;
 
 import com.linkedin.backend.features.authentication.model.AuthenticationUser;
 import com.linkedin.backend.features.authentication.repository.AuthenticationUserRepository;
+import com.linkedin.backend.features.authentication.utils.Encoder;
 
 @Configuration
 public class LoadDatabaseConfiguration {
 	
+	public final Encoder encoder;
+	
+	public LoadDatabaseConfiguration(Encoder encoder) {
+		this.encoder = encoder;
+	}
+	
 	@Bean
 	public CommandLineRunner initDatabase (AuthenticationUserRepository authenticationUserRepository) {
 		return args -> {
-			AuthenticationUser authenticationUser = new AuthenticationUser("puja@email.com", "mypassword");
+			AuthenticationUser authenticationUser = new AuthenticationUser("puja@email.com", encoder.encode("mypassword"));
 			authenticationUserRepository.save(authenticationUser);
 			AuthenticationUser authenticationUser2 = new AuthenticationUser("puja2@email.com", "mypassword2");
 			authenticationUserRepository.save(authenticationUser2);
---
+++ backend/src/main/java/com/linkedin/backend/features/authentication/utils/Encoder.java
@@ -0,0 +1,23 @@
+package com.linkedin.backend.features.authentication.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Encoder {
+	public String encode(String rawString) {
+		try {
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		return Base64.getEncoder().encodeToString(rawString.getBytes());
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("Error encoding String", e);
+		}
+	}
+	
+	public boolean matches(String rawString, String encodedString) {
+		return encode(rawString).equals(encodedString);
+	}
+}
