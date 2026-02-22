1335faf1ebe76ec0b515a33830a4722f9cbfdab5
PurnimaPataskar <pataskar.purnima@gmail.com>
2026-02-22 22:50:41 +0530

    secure endpoints to use token for request validation

---
+++ backend/src/main/java/com/linkedin/backend/features/authentication/filter/AuthenticationFilter.java
@@ -0,0 +1,80 @@
+package com.linkedin.backend.features.authentication.filter;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.linkedin.backend.features.authentication.model.AuthenticationUser;
+import com.linkedin.backend.features.authentication.service.AuthenticationService;
+import com.linkedin.backend.features.authentication.utils.JsonWebToken;
+
+import io.jsonwebtoken.lang.Arrays;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthenticationFilter extends HttpFilter {
+	
+	private final List<String> unsecuredEndpoints = List.of(
+		    "/api/v1/authentication/login",
+		    "/api/v1/authentication/register",
+		    "/api/v1/authentication/send-password-reset-token",
+		    "/api/v1/authentication/reset-password"
+		);
+	private final JsonWebToken jsonWebTokenService;
+	private final AuthenticationService authenticationService;
+	
+	public AuthenticationFilter(JsonWebToken jsonWebTokenService, AuthenticationService authenticationService) {
+		this.jsonWebTokenService = jsonWebTokenService;
+		this.authenticationService = authenticationService;
+	}
+	
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+		response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+		
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+			return;
+		}
+		
+		String path = request.getRequestURI();
+		
+		if(unsecuredEndpoints.contains(path)) {
+			chain.doFilter(request, response);
+			return;
+		}
+		
+		try {
+			String authorization = request.getHeader("Authorization");
+			
+			if(authorization == null || !authorization.startsWith("Bearer ")) {
+				throw new ServletException("Token missing.");
+			}
+			
+			String token = authorization.substring(7);
+			
+			if (jsonWebTokenService.isTokenExpired(token)) {
+				throw new ServletException("Invalid Token");
+			}
+			
+			String email = jsonWebTokenService.getEmailFromToken(token);
+			AuthenticationUser user = authenticationService.getUser(email);
+			request.setAttribute("authenticationUser", user);
+			chain.doFilter(request, response);
+			
+		} catch(Exception e) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.setContentType("application/json");
+			response.getWriter().write("{\"message\": \"Invalid authentication token, or token missing.\"}");
+			
+		}
+    }
+}
