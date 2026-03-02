97571fec3774b0933ce48989538281d37c453a7f
PurnimaPataskar <pataskar.purnima@gmail.com>
2026-03-02 17:21:49 +0530

    Add Poppins font, assets; switch to SCSS and deps
    Include Google Poppins font in index.html, add new public assets (favicon.ico, compass/map SVGs) and remove default Vite/react starter assets. Replace App files and plain CSS with an index.scss and update main.tsx to use it. Update frontend dependencies to add react-router-dom, types and Sass support and apply corresponding package-lock updates.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.scss'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home</h1>
+  },
+
+  {
+    path: "/login",
+    element: <h1>Login</h1> 
+  },
+
+  {
+    path: "/signup",
+    element: "Signup"
+  }
+
+  ,
+  {
+    path: "/reset-password",
+    element: "Reset Password"  
+  },
+
+  {
+    path: "verify-email",
+    element: "Verify Email"
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
