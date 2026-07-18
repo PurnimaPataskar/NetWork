cbec3d64b5efbcdb31df3f778d3c69747e163259
PurnimaPataskar <pataskar.purnima@gmail.com>
2026-07-18 19:07:05 +0530

    Use BackendApplication in SpringBootTest

--- backend/src/test/java/com/example/networks/BackendApplicationTests.java
+++ backend/src/test/java/com/example/networks/BackendApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.networks;
 
+import com.linkedin.backend.BackendApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BackendApplication.class)
 class BackendApplicationTests {
 
 	@Test
